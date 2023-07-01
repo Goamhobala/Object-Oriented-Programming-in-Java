62a2b248b1f7b905bb5f1e31315a6246dd0b841f
J Yapp <yehbebe0112@gmail.com>
2023-07-01 19:46:33 +0200

    Added notes for memory model

---
+++ code-along/src/code_along/TestMemoryModelPrimative.java
@@ -0,0 +1,13 @@
+package code_along;
+
+public class TestMemoryModelPrimative {
+	public static void main(String[] args) {
+		int var1;
+		var1 = 57;
+		int var2;
+		var2 = var1;
+		var1 = 123;
+		System.out.println("Var1: " + var1 + "\nVar2: " + var2);
+	}
+
+}
